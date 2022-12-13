ada0992421bb952e275c0f30d1a4437988a18dd0
unknown <jawadmansoor1998@gmail.com>
2022-12-14 00:48:14 +0500

    colors added

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,2 +1,10 @@
-const colors = {};
+const colors = {
+  GREEN_SHADE_A: '#71C9CE', //primary
+  GREEN_SHADE_B: '#A6E3E9',
+  GREEN_SHADE_C: '#CBF1F5',
+  GREEN_SHADE_D: '#E3FDFD',
+
+  WHITE: '#fff',
+  BLACK: '#000',
+};
 export default colors;
