ece1b9bb44ef12cb64ac7fa9945f61e3e6c5294a
unknown <jawadmansoor1998@gmail.com>
2022-12-13 23:48:43 +0500

    fontfiles added

--- src/theme/fontFamily.js
+++ src/theme/fontFamily.js
@@ -1,3 +1,11 @@
-const fontFamily = {};
+const fontFamily = {
+  LIGHT: 'Exo-Light',
+  REGULAR: 'Exo-Regular',
+  ITALIC: 'Exo-Italic',
+  MEDIUM: 'Exo-Medium',
+  BOLD: 'Exo-Bold',
+  SEMI_BOLD: 'Exo-SemiBold',
+  EXTRA_BOLD: 'Exo-ExtraBold',
+};
 
 export default fontFamily;
--- src/theme/fontSize.js
+++ src/theme/fontSize.js
@@ -1,2 +1,8 @@
-const fontSizes = {};
+const fontSizes = {
+  MINI: 10,
+  SMALL: 12,
+  MEDIUM: 14,
+  LARGE: 16,
+  EXTRA: 24,
+};
 export default fontSizes;
