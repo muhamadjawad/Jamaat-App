6e78f23acb5453ff837d4fff5308f372c94f4fba
muhamadjawad <jawadmansoor1998@gmail.com>
2022-12-15 15:01:03 +0500

    primary key added to color.js

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,8 +1,13 @@
+const GREEN_SHADE_A = '#71C9CE';//primary
+const GREEN_SHADE_D = '#E3FDFD' // tertiary
+const BLACK = '#000';
+
 const colors = {
-  GREEN_SHADE_A: '#71C9CE', //primary
+  PRIMARY: GREEN_SHADE_A, 
+  SECONDARY: BLACK,
+  TERNARY: GREEN_SHADE_D,
   GREEN_SHADE_B: '#A6E3E9',
   GREEN_SHADE_C: '#CBF1F5',
-  GREEN_SHADE_D: '#E3FDFD',
 
   WHITE: '#fff',
   BLACK: '#000',
