c0b0d81c2171d5941d968e916abc6f0a91f2317f
muhamadjawad <jawadmansoor1998@gmail.com>
2022-12-16 15:13:50 +0500

    gray color added

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,15 +1,16 @@
 const GREEN_SHADE_A = '#71C9CE';//primary
 const GREEN_SHADE_D = '#E3FDFD' // tertiary
-const BLACK = '#000';//secondary
+const BLACK = '#0f0f0f';//secondary
 
 const colors = {
-  PRIMARY: GREEN_SHADE_A, 
+  PRIMARY: GREEN_SHADE_A,
   SECONDARY: BLACK,
   TERNARY: GREEN_SHADE_D,
   GREEN_SHADE_B: '#A6E3E9',
   GREEN_SHADE_C: '#CBF1F5',
 
   WHITE: '#fff',
-  BLACK: '#000',
+  BLACK: '#0f0f0f',
+  GRAY: "#999999"
 };
 export default colors;
