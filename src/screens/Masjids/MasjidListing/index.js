a22d823a4844f379f61220ffa53597d036b29602
unknown <jawadmansoor1998@gmail.com>
2022-12-18 23:39:56 +0500

    search bar done

--- src/screens/Masjids/MasjidListing/index.js
+++ src/screens/Masjids/MasjidListing/index.js
@@ -1,10 +1,17 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import {View} from 'react-native';
+import SearchBar from '../../../components/SearchBar';
 import RenderMasjid from './RenderMasjid';
 
 const MasjidListing = () => {
+  const [searchResult, setSearchResult] = useState('');
   return (
     <View>
+      <SearchBar
+        placeholder="MAsjid Btaen"
+        value={searchResult}
+        onChangeText={val => setSearchResult(val)}
+      />
       {[1, 2, 3, 4, 5].map((item, index) => (
         <RenderMasjid key={index} />
       ))}
