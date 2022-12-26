878b695a729ab4ddd22d4fc043a27b1f2896d5d8
unknown <jawadmansoor1998@gmail.com>
2022-12-26 23:49:29 +0500

    tab bar added

--- src/screens/Events/index.js
+++ src/screens/Events/index.js
@@ -1,15 +1,20 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import Daroos from './Daroos';
 import {GLOBAL_SCREEN_PADDING} from '../../utils/logicalConstants';
 import Khutbaat from './Khutbaat';
 import colors from '../../theme/colors';
+import EventTabBar from './EventTabBar';
+import Conferences from './Conferences';
 
 const Events = () => {
+  const [activeEvent, setActiveEvent] = useState('khutbaat'); //'khutbaat'  or 'daroos' 'conference'
   return (
     <View style={[styles.main]}>
-      {/* <Daroos /> */}
-      <Khutbaat />
+      <EventTabBar activeEvent={activeEvent} setActiveEvent={setActiveEvent} />
+      {activeEvent === 'daroos' && <Daroos />}
+      {activeEvent === 'khutbaat' && <Khutbaat />}
+      {activeEvent === 'conferences' && <Conferences />}
     </View>
   );
 };
@@ -18,6 +23,7 @@ const styles = StyleSheet.create({
   main: {
     backgroundColor: colors.WHITE,
     paddingHorizontal: GLOBAL_SCREEN_PADDING,
+    flex: 1,
   },
 });
 export default Events;
